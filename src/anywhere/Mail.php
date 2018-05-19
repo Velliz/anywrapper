cdbe861c4ace6cbecabb21509a2c5456dc33f554
Didit Velliz <diditvelliz@outlook.com>
2018-05-19 14:26:39 +0700

    curl return transfer

--- src/anywhere/Mail.php
+++ src/anywhere/Mail.php
@@ -124,7 +124,7 @@ class Mail extends Wrapper
 
     /**
      * @param $apiUrl
-     * @return mixed|void
+     * @return mixed
      */
     public function Send($apiUrl)
     {
@@ -142,13 +142,16 @@ class Mail extends Wrapper
             curl_setopt($curl, CURLOPT_USERAGENT, 'Anywhere Wrapper');
             curl_setopt($curl, CURLOPT_TIMEOUT, 1);
             curl_setopt($curl, CURLOPT_HEADER, 0);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
             curl_setopt($curl, CURLOPT_DNS_CACHE_TIMEOUT, 10);
             curl_setopt($curl, CURLOPT_FRESH_CONNECT, true);
-            curl_exec($curl);
+            $content = curl_exec($curl);
             curl_close($curl);
+
+            return $content;
         }
+        return null;
     }
 }
