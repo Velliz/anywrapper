5966365dd434ea8c4e40b04564b8ffb49564e3d4
Didit Velliz <diditvelliz@gmail.com>
2021-06-18 10:06:30 +0700

    fix missing CURLOPT_RETURNTRANSFER for $do_die false

--- src/anywhere/Pdf.php
+++ src/anywhere/Pdf.php
@@ -120,9 +120,10 @@ class Pdf extends Wrapper
 
             echo $response;
             die();
-        } else {
-            $response = curl_exec($curl);
         }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
 
         return $response;
     }
